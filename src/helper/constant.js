a9483a9b9d6c136cf4f970dbdfe03e16db503487
SnowFox <kwebdev225@gmail.com>
2021-02-05 11:47:48 +0800

    Add comments

--- src/helper/constant.js
+++ src/helper/constant.js
@@ -1,4 +1,5 @@
 const APP_CONST = {
+  /** URL Constants */
   API_URL: "http://127.0.0.1:5000/api",
   // API_URL: 'http://zed.umbrellaink.com.au/api',
   // BASE_URL: 'http://zed.umbrellaink.com.au',
@@ -15,7 +16,7 @@ const APP_CONST = {
     OR: '#FFA500', SA: '#c2b280', BL: '#0000ff'
   },
   GENDER_LIST: [
-    { key: 'M', name: 'Mens' },
+    { key: 'M', name: 'Mens' }, 
     { key: 'F', name: 'Womens' }
   ],
   THEME: ['L', 'D'],
