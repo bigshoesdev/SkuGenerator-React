fb622c7bc6aed6657743287cf60aa1f556bdafa9
SnowFox <kwebdev225@gmail.com>
2021-02-07 00:55:41 +0800

    Clean code

--- src/helper/constant.js
+++ src/helper/constant.js
@@ -1,5 +1,4 @@
 const APP_CONST = {
-  /** URL Constants */
   API_URL: "http://127.0.0.1:5000/api",
   // API_URL: 'http://zed.umbrellaink.com.au/api',
   // BASE_URL: 'http://zed.umbrellaink.com.au',
